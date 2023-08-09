e82f97d9d272b7541123f587bcbf94c4fd8df558
achref_ama <amami.achref@gmail.com>
2023-08-09 13:56:19 +0100

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VentesComponent } from './ventes/ventes.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VentesComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,34 @@
+import { Component,OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss']
+})
+export class ArticleComponent {
+  
+  @Input()
+  Des_Article!: string;
+
+  @Input()
+  Prix_Article!: string;
+  @Input()
+  Qte_Article!: string;
+
+  getQteArticle()
+{
+  return this.Qte_Article;
+}
+getColor()
+{
+  if (this.Qte_Article ==="")
+  {
+    return 'red';
+  }
+  else
+  {
+    return 'green';
+  }
+}
+
+}
